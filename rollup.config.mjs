d2d29d1c028181ee9a5ef2d7f000c246f5b33878
Hiroki Osame <hiroki.osame@gmail.com>
2023-05-17 19:27:04 +0900

    build: upgrade rollup plugins

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,5 +1,5 @@
-import babel from 'rollup-plugin-babel';
-import { terser } from 'rollup-plugin-terser';
+import babel from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
 import filesize from 'rollup-plugin-filesize';
 
 const isProd = process.env.NODE_ENV === 'production';
